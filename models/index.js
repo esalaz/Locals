d9473003d51db95061fda52e0026fc5da0e577b4
esalaz415 <esalaz415@gmail.com>
2018-03-16 11:53:19 -0700

    created userModel linked and exported

--- models/index.js
+++ models/index.js
@@ -2,3 +2,5 @@ var mongoose = require('mongoose');
 /*add you connection somewhere here*/
 mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/Locals', {promiseLibrary: global.Promise});
 
+/* adding model UserModel to index.js */
+module.exports.UserModel = require('./UserModel');
