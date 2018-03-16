3dcf82b29e1b7d46bf5c9569cb35dcbf79453b60
esalaz415 <esalaz415@gmail.com>
2018-03-16 13:16:14 -0700

    added facebook strategy to passport.js

--- config/passport.js
+++ config/passport.js
@@ -51,38 +51,4 @@ module.exports = function(passport){
     });
   }));
 
-}
-There's a lot going on here so let's break it down. This code is really similar to what we did to use passport-local:
-
-First, we give the credentials for the current app to the Facebook strategy;
-Then, with the array given to profileFields, we describe which fields we want to get back from Facebook;
-The function that follows will be executed when Facebook sends back the data to the website using /auth/facebook/callback endpoint;
-Finally, if the user already exists, the code directly executes the callback and gives the user object found by mongo to the callback, otherwise, we create a new user object and execute the callback.
-Add the routes and the views
-To authenticate via OAuth with Facebook, an app needs three routes:
-
-A route to request Facebook
-A route for the Facebook callback
-A route for the logout
-For simplicity sake, we will set up just one view that shows different data depending on whether or not the user is logged in or not. In layout.ejs, add:
-
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Facebook authentication</title>
-</head>
-<body>
-  <h1>FACEBOOK LOGIN USING PASSPORT</h1>
-  <div>
-    <% if(user != undefined){ %>
-      <h2>Below is the data sent by facebook</h2>
-      <pre>
-        <%= user.fb %>
-      </pre>
-      <a href="/logout">Logout</a>
-    <% } else { %>
-      <a href="/auth/facebook">Login with Facebook</a>
-    <% } %>
-  </div>
-</body>
-</html>
+};
