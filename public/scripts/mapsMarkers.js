b2dfaa713a6bac4eb7134dac94c87fabc95c6c4b
esalaz415 <esalaz415@gmail.com>
2018-03-18 17:03:56 -0700

    undid nav bar to redesign

--- public/scripts/mapsMarkers.js
+++ public/scripts/mapsMarkers.js
@@ -1,5 +1,10 @@
 console.log("ready?!");
 function initMap() {
+
+  var markerIcons = {
+    flag: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+    otherflag: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
+  };
   var homeCity = {
     lat: 37.77493,
     lng: -122.419416
@@ -16,20 +21,21 @@ function initMap() {
     lat: 37.799263,
     lng: -122.397673
   }];
-
   var map = new google.maps.Map(document.getElementById('map'), {
     center: homeCity,
-    zoom: 12
+    zoom: 12,
   });
   var usersMarker = new google.maps.Marker({
     position: myLatLng[0],
     map: map,
-    title: "Home!"
+    title: "Home!",
+    icon: markerIcons.flag
   });
   var usersMarker2 = new google.maps.Marker({
     position: myLatLng[1],
     map: map,
-    title: "Home!"
+    title: "Home!",
+    icon: markerIcons.otherflag
   });
   var usersMarker3 = new google.maps.Marker({
     position: myLatLng[2],
@@ -37,4 +43,10 @@ function initMap() {
     title: "Home!"
   });
 
+  var infoWindow = new google.maps.InfoWindow({
+    content: '<h1>Bernal Heights</h1><p> Come out and enjoy the amazing views of the city with a local!</p>'
+  });
+  usersMarker.addListener('click', function(){
+    infoWindow.open(map, usersMarker);
+  });
 }
