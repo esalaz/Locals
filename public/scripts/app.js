70794508d61afb94a6528e31790e5341b3ce2873
t-tullis <t.tullis34@gmail.com>
2018-03-20 12:43:28 -0700

    Fixed fullpage Slider

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,24 +1,10 @@
 console.log('yoooo')
 
 //jQuery is required to run this code
-app.use(express.static(path.join(__dirname, 'client')));
+// app.use(express.static(path.join(__dirname, 'client')));
+
 $( document ).ready(function() {
     $('select').material_select();
-    // $('.carousel').carousel()
-
-    // $('.carousel').carousel('next');
-    // $('.carousel').carousel('next', 3); // Move next n times.
-
-    // $('.carousel.carousel-slider').carousel({fullWidth: true});
     $('.slider').slider();
   });
 
-
-function openMe(inside) {
-  var i, content;
-  content = document.getElementsByClassName("content");
-  for (i = 0; i < content.length; i++) {
-    content[i].style.display = "none";
-  }
-  document.getElementById(inside).style.display = "block";
-}
